c957f0f5d26d4489eed293c3143fe375580161cd
Basit Ng <basitng2004@gmail.com>
2022-10-09 08:41:57 +0100

    removed hyperlink

--- src/pages/solutions/enterprise-network-infrastructure.js
+++ src/pages/solutions/enterprise-network-infrastructure.js
@@ -12,7 +12,7 @@ export default function EnterpriseNetwoekInf() {
       title:
         "Wired Data / Voice CablingWireless LAN, Wireless Campus LAN Backbone and Backhaul Solution",
       id: nanoid(),
-      content: `Hyperlink Technologies has garnered huge experience in deploying wireless solutions ranging from wireless LAN to wireless LAN bridging in campus environment and metropolitan wireless backhaul for Data, Voice and Video transmission.
+      content: `Wancore Systems has garnered huge experience in deploying wireless solutions ranging from wireless LAN to wireless LAN bridging in campus environment and metropolitan wireless backhaul for Data, Voice and Video transmission.
 
       Mostly, we provide wireless LAN in areas where wired LAN is totally impossible or mobility is of high priority or as a backup to wired LAN. Also, in the campus environment where laying of Optical Fiber backbone cables is found to be very expensive to achieve or time of delivery will not allow the laying of Optical Fiber cables, we deploy wireless LAN bridging solution.
 
@@ -31,7 +31,7 @@ export default function EnterpriseNetwoekInf() {
       
       We have trained and seasoned Optical Fiber technician that gives your Optical Fiber cables best treatment from laying/blowing to splicing with acceptable international standard in mind always.
       
-      GSM and Mobile internet service providers in their plight to improve their services often result to laying Optical Fiber across the metropolis. Hyperlink Technologies is well positioned to provide them consultancy services, project management services or the actual implementation of the project.`,
+      GSM and Mobile internet service providers in their plight to improve their services often result to laying Optical Fiber across the metropolis. Wancore System is well positioned to provide them consultancy services, project management services or the actual implementation of the project.`,
       icon: <WifiOutlined sx={{ color: "#fff" }} />,
       color: "--color-pink-dark",
       backgroundColor: "--color-pink",
@@ -41,7 +41,7 @@ export default function EnterpriseNetwoekInf() {
       id: nanoid(),
       content: `Our structured cabling system provides a universal platform upon which an overall information systems strategy is built.
         With a flexible cabling infrastructure, support for multiple voice, data, video and multimedia systems regardless of their manufacturer is achievable. Wired in a star topology, each workstation links to a central point and facilitates system interconnection and administration. This approach allows communication with virtually any device, anywhere, at any time.
-        Through the years, Hyperlink Technologies has gone into alliances with major global cable manufacturing companies. The business model of the company is anchored on partnering with the best-in-class global companies, for their best products and services and delivering them to our customers in the best manner, using the best people.`,
+        Through the years, Wancore System has gone into alliances with major global cable manufacturing companies. The business model of the company is anchored on partnering with the best-in-class global companies, for their best products and services and delivering them to our customers in the best manner, using the best people.`,
       icon: <WifiOutlined sx={{ color: "#fff" }} />,
       color: "--color-red",
 
